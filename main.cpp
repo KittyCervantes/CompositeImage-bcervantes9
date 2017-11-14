58ed775b2568d97b698f7bf0d25f85ed36a311ae
KittyCervantes <bcervantes9@ecc-jaguar.csuchico.edu>
2017-11-13 22:04:42 -0800

    bones

--- main.cpp
+++ main.cpp
@@ -6,80 +6,105 @@
 
 using namespace std;
 
-bmp askFile();
-//asks for valid bmp image
-vector< vector<Pixel> >imageStack(& *.bmp);
-//matrix of images
-void imageAvg(Pixel &);
-//averages image values
+vector<Pixel>askFile(int &); //asks for valid bmp image
+Bitmap imageStack(vector<Pixel> &); //matrix of images
+void imageAvg(Pixel &); //averages image values
 
 
-int Max=10;
-//max number of images
+int MAX=10; //max number of images
+int MIN=2; //min number of images
 
 int main()
 {
-        int input;
+        string input;
         int count=0;
+        vectore<Pixel>album;
 
-        vector<Pixel>mashUp;
-
-        //Loop receives max of 9 other images
+        //Loop receives max of 10 images
         do{
-                askFile();
-                //asks for valid bmp file
-                count++;
-                //image count increases
+                album = askFile(count); //asks for valid bmp file, makes vector                
+                count++; //image count increases each loop
 
-                cout<<"Add another file? (Y)es (N)o"<<endl;
+                cout<<"Press any key to continue adding files or enter (DONE) to stop."<<endl;
                 cin>>input; 
-                //gives option to add more or not
 
-
-                if(input=N || input=n && count<2)
+                if(input= "DONE" || input= "done" && count<MIN)
                 {
-                        cout<<"Need minimum of 2 valid images!"<<endl;
-                        //makes sure theres a minimum of 2 given images
+                        cout<<"Need minimum of 2 valid images!"<<endl; //minimum of 2 images
                 }
 
-        }while(input= Y || input= y && count<MAX); //still not sure i wanna use a dowhile for this
+                if(count=MAX)
+                {
+                        cout<<"MAX number of image inputs reached"<<endl;
+                }
+        }while(input!= "DONE" || input!= "done" && count<MAX); //still not sure about dowhile
 
+        imageStack(album);
 
-        //creates matrix of images
-        vector<vector<Pixel> >imageStack;
-
-        //takes average of images
-
-
-        //save file
+        //save file ---------------change bmpimage and mashUp to appropriate variables later
         bmpimage.fromPixelMatrix(mashUp);
         bmpimage.save("composite-bcervantes9.bmp");
 
 
         return 0;
 }
+//------------------
+vector<Pixel>askFile(int & count)
+{       vector<Pixel>mashUp;
+        Bitmap imageFile;
+        string file;
 
-bitmap askFile()
+        cout<<"Enter valid 24 bit bmp image file name:"<<endl;
+        cin>>file;//asks for file name
+        imageFile.open(file);//opens image
+
+        //checks for valid format, prints error otherwise
+        if(imageFile.isImage() == false)
+        {
+                cerr<<"----!!Invalid file!!----"<<endl;
+                if(imageFile.isImage(.bmp) == false)
+                {
+                        cerr<<"!!Not a valid bmp image!!"<<endl;
+                }
+                mashUp.pop_back();//removes offending file
+                count--;//returns to previous count value
+        }
+        imageFile.toPixelMatrix();//converts to bitmap
+        mashUp.push_back(imageFile);//pushes into matrix
+
+        return mashUp;//returns image vector
+}
+//------------------next function
+Bitmap imageStack(vector & album)
 {
-        //ask user for image file
-        //saves user input
-        //checks input to make sure its a valid format
-        //print error if invalid
-        //returns images to pixel matrix mashUp
+        int pages=album.size();
+        int pix;
+vector<Pixel>photo;
+       for(i=0; i<pages;i++)
+        {
+
+                for(row=0; row<pages;row++)
+                {
+                        for(column=0;column<pages;column++)
+                        {
+                                pix += album[row][column];
+
+                        }
+                }
+
+
+        }
+        return ;
 }
 
-vector< vector<Pixel> >imageStack( bitmap & pic)
+//------------------next function
+void imageAvg( )
 {
-        //goes through each individual picture
-        //goes through each pixel
-        //adds respective pixel values
-        //calls imageAvg
-}
+        int focus;
+        //int pages=album.size();
 
-void imageAvg (Pixel & dot)
-{
-        //takes average //<-----------  HOW DOES IT KNOW THE NUMBER TO DIVIDE IT BY?  divide by vector.size() maybe? DIVIDE BY COUNT!!! It's keeping track of how many images you have inputed already!
-        //give new value
-        //returns new picture
+
+
+
 }
 
